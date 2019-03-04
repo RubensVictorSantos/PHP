b291e1eb5a518bc70464b5b6447da5594115fbe5
Rubens Victor <rubensvictorsantos@gmail.com>
2019-03-04 18:35:00 -0300

    versão 01 04/03 18:34

---
+++ PROJETO/Sobre.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once("menu.php");
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Sobre a empresa
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div id="conteudo" class="center">
+            <div >
+                <p>
+                EMPRESAA Realbras atua no mercado de terceirização Industrial, montagem, acondicionamento e desenvolvimento de soluções tecnológicas para a indústria em geral e agências de marketing.Fundada em agosto de 2004 com o nome RealGraf, na cidade de Jandira/ SP, é voltada para o mercado de montagem de válvulas Pump, montagem de kits promocionais e impressão de dados variáveis.Tudo começou em uma sala de 30 m² com apenas dez funcionários para retrabalhar alguns materiais de cosméticos, oriundos de importação e com isso, após percepção de carência nesta área, foram dados os primeiros passos no ramo de terceirização industrial. Aos poucos a empresa foi crescendo em número de funcionários e também em suas áreas operacionais, o que tornou possível atender outro mercado. O objetivo era a empresa fornecer diversas linhas de trabalhos em um único local, visando qualidade, tempo e menor custo, partindo para armazenagem, industrialização e controles de entregas.Em meados de 2007, mesmo em um mercado restrito a multinacionais, houve a oportunidade de atuação na área de cartões GSM, garantindo segurança e qualidade. A tecnologia e toda a estrutura de montagem e validação dos cartões foram desenvolvidas pela RealGraf, sendo uma das pioneiras neste segmento. Atualmente, atende todas as bandeiras de cartões GSM.Em 2014, a RealGraf completa 10 anos de mercado e dado seu crescimento e sua evolução a empresa está passando por uma reestruturação adotando o nome Realbras. A mudança de nome veio através de estudos mostrando que o nome com final GRAF estava nos direcionando para um segmento específico do mercado gráfico.Em todos esses anos, a Realbras sempre procurou uma evolução contínua para acompanhar o mercado atual e entregar o produto final do cliente visando a máxima qualidade. A empresa atende todos os SLAs com controles rigorosos de qualidade e investe em software de gestão e equipamentos, procurando sempre surpreender seus clientes e a cada dia se tornar uma empresa conceituada no mercado.EMPRESAA Realbras atua no mercado de terceirização Industrial, montagem, acondicionamento e desenvolvimento de soluções tecnológicas para a indústria em geral e agências de marketing.Fundada em agosto de 2004 com o nome RealGraf, na cidade de Jandira/ SP, é voltada para o mercado de montagem de válvulas Pump, montagem de kits promocionais e impressão de dados variáveis.Tudo começou em uma sala de 30 m² com apenas dez funcionários para retrabalhar alguns materiais de cosméticos, oriundos de importação e com isso, após percepção de carência nesta área, foram dados os primeiros passos no ramo de terceirização industrial. Aos poucos a empresa foi crescendo em número de funcionários e também em suas áreas operacionais, o que tornou possível atender outro mercado. O objetivo era a empresa fornecer diversas linhas de trabalhos em um único local, visando qualidade, tempo e menor custo, partindo para armazenagem, industrialização e controles de entregas.Em meados de 2007, mesmo em um mercado restrito a multinacionais, houve a oportunidade de atuação na área de cartões GSM, garantindo segurança e qualidade. A tecnologia e toda a estrutura de montagem e validação dos cartões foram desenvolvidas pela RealGraf, sendo uma das pioneiras neste segmento. Atualmente, atende todas as bandeiras de cartões GSM.Em 2014, a RealGraf completa 10 anos de mercado e dado seu crescimento e sua evolução a empresa está passando por uma reestruturação adotando o nome Realbras. A mudança de nome veio através de estudos mostrando que o nome com final GRAF estava nos direcionando para um segmento específico do mercado gráfico.Em todos esses anos, a Realbras sempre procurou uma evolução contínua para acompanhar o mercado atual e entregar o produto final do cliente visando a máxima qualidade. A empresa atende todos os SLAs com controles rigorosos de qualidade e investe em software de gestão e equipamentos, procurando sempre surpreender seus clientes e a cada dia se tornar uma empresa conceituada no mercado.
+                
+                </p>
+            </div>
+        
+        </div>
+    </body>
+</html>
+<?php
+
+    require_once("footer.php");
+
+?>
