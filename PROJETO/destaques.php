3d67160541b6822f29345631adf1272c2f9c8c39
Rubens Victor <rubensvictorsantos@gmail.com>
2019-03-11 19:04:46 -0300

    versão 01 11/03 19:04

--- PROJETO/destaques.php
+++ PROJETO/destaques.php
@@ -14,6 +14,65 @@
         ?>
         <div id="conteudo" class="center" >
         
+            <div id="principais-noticias">
+                <div class="box-noticia">
+                    <img src="img/002.jpg" alt="" class="img-main-noticia" title="" >
+                </div>
+                <div class="box-noticia-titulo"  style="position:absolute; background-color:rgba(255,187,51,0.6); margin: 0px px 0px 0px;">
+                
+                </div>
+                <div class="box-noticia">
+                    <img src="img/001.jpg" class="img-main-noticia" alt="" title="" >
+                </div>
+                <div class="box-noticia-titulo" style="position:absolute; background-color:rgba(255,187,51,0.6); margin: 0px 0px 0px 0px;">
+                
+                </div>
+                <div class="box-noticia">
+                    <img src="img/004.jpg" alt="" class="img-main-noticia" title="" >
+                </div>
+                <div class="box-noticia-titulo" style="position:absolute; background-color:rgba(255,187,51,0.6); margin: 0px 0px 0px 0px;">
+                
+                </div>
+            </div>
+            <div class="noticias">
+                <div class="conteudo-noticia">
+                    <div class="box-img-noticia">
+                    
+                    </div>
+                    <div >
+                    
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+            <div class="noticias">
+                <div class="conteudo-noticia">
+                    <div class="box-img-noticia">
+                    
+                    </div>
+                    <div>
+                    
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+            <div class="noticias">
+                <div class="conteudo-noticia">
+                    <div class="box-img-noticia">
+                    
+                    </div>
+                    <div>
+                    
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
         
         </div>        
         <?php 
