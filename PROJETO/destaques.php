a0e969166d1b28beb513614bb17eddf169d63933
Rubens Victor <rubensvictorsantos@gmail.com>
2019-03-12 10:56:24 -0300

    versão 01 12/03 10:56/

--- PROJETO/destaques.php
+++ PROJETO/destaques.php
@@ -17,29 +17,11 @@
             <div id="principais-noticias">
                 <div class="box-noticia">
                     <img src="img/002.jpg" alt="" class="img-main-noticia" title="" >
-<<<<<<< HEAD
-                </div>
-                <div class="box-noticia-titulo"  style="position:absolute; background-color:rgba(255,187,51,0.6); margin: 0px px 0px 0px;">
-                
-                </div>
-                <div class="box-noticia">
-                    <img src="img/001.jpg" class="img-main-noticia" alt="" title="" >
-                </div>
-                <div class="box-noticia-titulo" style="position:absolute; background-color:rgba(255,187,51,0.6); margin: 0px 0px 0px 0px;">
-                
-                </div>
-                <div class="box-noticia">
-                    <img src="img/004.jpg" alt="" class="img-main-noticia" title="" >
-                </div>
-                <div class="box-noticia-titulo" style="position:absolute; background-color:rgba(255,187,51,0.6); margin: 0px 0px 0px 0px;">
-                
-                </div>
-=======
                     
                     <div class="box-noticia-titulo" style="">
                         <a href="#" style="text-decoration: none;color:white;">
                             <h1>
-                                TEST TEST TEST
+                                Nasa Manda a 1° Bike para a Lua.
                             </h1>
                         </a>
                     </div>
@@ -51,7 +33,7 @@
                     <div class="box-noticia-titulo" style="">
                         <a href="#" style="text-decoration: none;color:white;">
                             <h1>
-                                TEST TEST TEST
+                                Campeonato Mundial de Bikes 14/03
                             </h1>
                         </a>
                     </div>
@@ -63,7 +45,7 @@
                         <div class="box-noticia-titulo" style="">
                             <a href="#" style="text-decoration: none;color:white;">
                                 <h1>
-                                    TEST TEST TEST
+                                    O Gerente Enlouqueceu!, 30% de Desconto em Tudo!
                                 </h1>
                             </a>
                         </div>
@@ -71,109 +53,78 @@
                 
             </div>
             <div class="noticias">
-                <div class="conteudo-noticia">
-                    <div class="box-img-noticia">
-                        <img src="img/004.jpg" alt="" class="img-main-noticia" title="" >
+                <a href="#" class="links-noticia">
+                    <div class="conteudo-noticia">
+                        <div class="box-img-noticia">
+                            <img src="img/bike-presidente.jpg" alt="" class="img-noticia-conteudo" title="" >
+                        </div>
+                        <div class="text-noticia">
+                            <h1 class="formatacao-titulo-noticia">veja a última tendência para bicicletas presidenciais</h1>
+                            <p class="formatacao-texto-destaque">
+                            </p>
+                            <p>
+                                20/04/2019
+                            </p>
+                        </div>
+
                     </div>
-                    <div class="text-noticia">
-                        <h1>TEST TEST TEST TEST TEST TEST TEST TEST</h1>
-                        <p>
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                        </p>
-                    </div>
-                
-                </div>
-            
->>>>>>> c57d0d9079c83b179fde9f13b6f42f834901c401
+                </a>
             </div>
             <div class="noticias">
-                <div class="conteudo-noticia">
-                    <div class="box-img-noticia">
-<<<<<<< HEAD
-                    
+                <a href="#" class="links-noticia">
+                    <div class="conteudo-noticia">
+                        <div class="box-img-noticia">
+                            <img src="img/bike-pet.jpg" alt="" class="img-noticia-conteudo" title="" >
+                        </div>
+                        <div class="text-noticia">
+                            <h1 class="formatacao-titulo-noticia">Bicicleta de garrafa pet </h1>
+                            <p class="formatacao-texto-destaque">
+                                Morador de rua após uma epifania desenvolve sozinho uma bicicleta de garrafa pet, de acordo com ele o mais difícil foi soldar as garrafas...
+                            </p>
+                            <p>
+                                20/04/2019
+                            </p>
+                        </div>
+
                     </div>
-                    <div >
-                    
-                    
-=======
-                        <img src="img/004.jpg" alt="" class="img-main-noticia" title="" >
-                    </div>
-                    <div class="text-noticia">
-                        <h1>TEST TEST TEST TEST TEST TEST TEST TEST</h1>
-                        <p>
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                        </p>
->>>>>>> c57d0d9079c83b179fde9f13b6f42f834901c401
-                    </div>
-                
-                </div>
-            
+                </a>
             </div>
             <div class="noticias">
-                <div class="conteudo-noticia">
-                    <div class="box-img-noticia">
-<<<<<<< HEAD
-                    
+                <a href="#" class="links-noticia">
+                    <div class="conteudo-noticia">
+                        <div class="box-img-noticia">
+                            <img src="img/bike-papa.jpg" alt="" class="img-noticia-conteudo" title="" >
+                        </div>
+                        <div class="text-noticia">
+                            <h1 class="formatacao-titulo-noticia">Depois do papamóvel conheça a nova papabike</h1>
+                            <p class="formatacao-texto-destaque">
+                            </p>
+                            <p>
+                                19/04/2019
+                            </p>
+                        </div>
+
                     </div>
-                    <div>
-                    
-                    
-=======
-                        <img src="img/004.jpg" alt="" class="img-main-noticia" title="" >
-                    </div>
-                    <div class="text-noticia">
-                        <h1>TEST TEST TEST TEST TEST TEST TEST TEST</h1>
-                        <p>
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                        </p>
->>>>>>> c57d0d9079c83b179fde9f13b6f42f834901c401
-                    </div>
-                
-                </div>
-            
+                </a>
             </div>
             <div class="noticias">
-                <div class="conteudo-noticia">
-                    <div class="box-img-noticia">
-<<<<<<< HEAD
-                    
+                <a href="#" class="links-noticia">
+                    <div class="conteudo-noticia">
+                        <div class="box-img-noticia">
+                            <img src="img/uber-bikes.jpg" alt="" class="img-noticia-conteudo" title="" >
+                        </div>
+                        <div class="text-noticia">
+                            <h1 class="formatacao-titulo-noticia">Saiba como ser um Uber bike </h1>
+                            <p class="formatacao-texto-destaque">
+                                Com o almento de transito em são paulo uber lança uma nova forma de carona conheça o uber bike
+                            </p>
+                            <p>
+                                19/04/2019
+                            </p>
+                        </div>
+
                     </div>
-                    <div>
-                    
-                    
-=======
-                        <img src="img/004.jpg" alt="" class="img-main-noticia" title="" >
-                    </div>
-                    <div class="text-noticia">
-                        <h1>TEST TEST TEST TEST TEST TEST TEST TEST</h1>
-                        <p>
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                            TEST TEST TEST TEST TEST TEST TEST TEST
-                        </p>
->>>>>>> c57d0d9079c83b179fde9f13b6f42f834901c401
-                    </div>
-                
-                </div>
-            
+                </a>
             </div>
         
         </div>        
