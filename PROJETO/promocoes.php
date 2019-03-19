36a895759703ebc4bd4c661eb9827a4b2575ad5a
Rubens Victor <rubensvictorsantos@gmail.com>
2019-03-19 10:53:09 -0300

    versão 01 19/03 10:53

--- PROJETO/promocoes.php
+++ PROJETO/promocoes.php
@@ -14,7 +14,197 @@
 
         ?>
         <div id="conteudo" class="center">
-        
+            
+            <div id="box-catalogo-promo">
+                    <div class="card-box-promo">
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeCaloiLotus.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                   Mountain Bike Caloi Lotus
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 29 Freio a Disco 21 Marchas Feminina
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+
+                                <a href="#">Detalhes</a>
+
+                            </div>
+                        </div>
+                        
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeCaloiLotus.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                   Mountain Bike Caloi Lotus
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 29 Freio a Disco 21 Marchas Feminina
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+
+                                <a href="#">Detalhes</a>
+
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeTrackBikesTKS.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                    Mountain Bike Track Bikes TKS
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 29 Freio a Disco Câmbio Shimano 21 Marchas
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+
+                                <a href="#">Detalhes</a>
+
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeCaloiSport.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                    Mountain Bike Caloi Aluminum Sport
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 26 Freio V-Brake 21 Marchas
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+                                <a href="#">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="card-box-promo">
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeCaloiLotus.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                   Mountain Bike Caloi Lotus
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 29 Freio a Disco 21 Marchas Feminina
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+
+                                <a href="#">Detalhes</a>
+
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeTrackBikesTKS.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                    Mountain Bike Track Bikes TKS
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 29 Freio a Disco Câmbio Shimano 21 Marchas
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+
+                                <a href="#">Detalhes</a>
+
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="img-card center">
+                                <img src="img/imgbikes_MountainBikeCaloiSport.jpg" alt="" class="img-card">
+                            </div>
+                            <div class="nome-card">
+                                <p>
+                                    Mountain Bike Caloi Aluminum Sport
+                                </p>
+                            </div>
+                            <div class="desc-card">
+                                <p>
+                                    Aro 26 Freio V-Brake 21 Marchas
+                                </p>
+                            </div>
+                            <div class="preco-card">
+                                <p>
+                                    R$854,05
+                                    10x de R$85,41
+                                </p>
+                            </div>
+                            <div class="detalhes">
+                                <a href="#">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         
         </div>
         <?php
